bb6c183a5fbd53d11a3a8dc5fb78b13b6e204995
lzferreira <luiz.analista@hotmail.com>
2019-12-11 16:15:23 -0300

    novas alterações

--- node/src/controllers/ProductController.js
+++ node/src/controllers/ProductController.js
@@ -4,9 +4,30 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res){
-        const products = await Product.find();
+        const { page } = req.query;
+        const products = await Product.paginate({}, {page: page, limit:10});
 
         return res.json(products);
+    },
+
+    async show(req, res){
+        const product = await Product.findById(req.params.id);
+        return res.json(product);
+    },
+
+    async store(req, res){
+        const product = await Product.create(req.body);
+        return res.json(product);
+    },
+
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new :true});
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id    );
+        return res.send("Removido com sucesso!");
     }
 
 };
