a1febc701fb790a6c10ffa27f48e695dad6350ed
lzferreira <luiz.analista@hotmail.com>
2019-12-12 16:42:44 -0300

    Cors

--- node/server.js
+++ node/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //mongoose.connect('mongodb+srv://lzferreira:lzferreira@clusterteste-gpu6n.mongodb.net/test?retryWrites=true&w=majority', {
 //    useNewUrlParser:true,
